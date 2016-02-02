df03ddc3aa35977f49354c71fabcb32338c3a5c1
Al Nejati <anej001@aucklanduni.ac.nz>
2016-02-03 11:26:51 +1300

    More comments for type parameters

--- AffineSpaces.jl
+++ AffineSpaces.jl
@@ -1,6 +1,9 @@
 using FixedSizeArrays
 
 # An affine space defined as all x satisfying Tx = b
+# Here, N is the dimension of the 'parent' or 'embedding' space,
+# M is the dimension of the affine space (e.g. 0 for point, 1 for line, etc.)
+# and T is just the base number field.
 immutable AffineSpace{T,N,M}
 	T::Mat{M,N,T}   # transformation (must be part of an orthonormal transformation)
 	b::Vec{M,T}     # offset
