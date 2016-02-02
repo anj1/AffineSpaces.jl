9f4d0610dc771bd14d5e95da009555baae507cfe
Al Nejati <anej001@aucklanduni.ac.nz>
2016-02-03 10:28:24 +1300

    Update util.jl

--- util.jl
+++ util.jl
@@ -1,8 +1,8 @@
 # distance between two affine spaces
 # due to Gross & Trenkler, and DuPre & Kass (1992)
 function dist_affine{T,N}(as1::AffineSpace{T,N},as2::AffineSpace{T,N})
-  A,a = as1.T,as1.b
-  B,b = as2.T,as2.b
+	A,a = as1.T,as1.b
+	B,b = as2.T,as2.b
   
 	C = nullspace(hcat(nullspace(B),nullspace(A))')
 	ortho_proj = C*pinv(C)
