1e6714bbc345b862378a967c8a87461f023a1c5c
Al Nejati <anej001@aucklanduni.ac.nz>
2016-02-03 10:42:34 +1300

    Update aliases.jl

--- aliases.jl
+++ aliases.jl
@@ -1,1 +1,10 @@
+# some useful abstractions and special cases.
 
+# n-dimensional point.
+Point{T,N}(x0::Vec{N,T}) = AffineSpace(Mat(eye(N)), x0)
+
+# line in 2 dimensions, given by starting point and vector
+function Line2D{T}(p0::Point{T,2}, v::Vec{2,T})
+  fv = Vec([v[2],-v[1]])
+  AffineSpace(Mat(v0)', dot(p0,v0))
+end
