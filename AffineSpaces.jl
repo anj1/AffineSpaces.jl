5eb472114fe18b43d1164113b26746648d78f281
Al Nejati <anej001@aucklanduni.ac.nz>
2016-02-03 12:12:43 +1300

    Update AffineSpaces.jl

--- AffineSpaces.jl
+++ AffineSpaces.jl
@@ -2,7 +2,8 @@ using FixedSizeArrays
 
 # An affine space defined as all x satisfying Tx = b
 # Here, N is the dimension of the 'parent' or 'embedding' space,
-# M is the dimension of the affine space (e.g. 0 for point, 1 for line, etc.)
+# M is the number of 'free' dimensions of the affine space
+#  (e.g. N for point, N-1 for line, 1 for hyperplane, etc.)
 # and T is just the base number field.
 immutable AffineSpace{T,N,M}
 	T::Mat{M,N,T}   # transformation (must be part of an orthonormal transformation)
