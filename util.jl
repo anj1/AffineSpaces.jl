d7d62a60f2dfa188ff7a55f07ad0b055a0180ee5
Al Nejati <anej001@aucklanduni.ac.nz>
2016-02-03 12:10:20 +1300

    added signed distance

--- util.jl
+++ util.jl
@@ -8,3 +8,6 @@ function dist_affine{T,N}(as1::AffineSpace{T,N},as2::AffineSpace{T,N})
 	ortho_proj = C*pinv(C)
 	norm(ortho_proj*(pinv(A)*a - pinv(B)*b))
 end
+
+# signed distance between a half-space and a point
+signed_distance{T,N}(hs::HalfSpace{T,N}, p::Point{T,N}) = dot(hs.n, p.b) + hs.a
